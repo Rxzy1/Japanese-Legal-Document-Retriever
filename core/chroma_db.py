64f0c9b3e924cbb4bff973ed52fd4e431969051d
Tushar Mudgal <tusharmudgal669@gmail.com>
2026-04-13 17:25:33 +0530

    added vector_storage using chromadb

---
+++ core/chroma_db.py
@@ -0,0 +1,17 @@
+import chromadb
+class store_vector:
+    def __init__(self):
+        self.client = chromadb.PersistentClient(path=r"C:\Project\Vectors")
+        self.collection = self.client.get_or_create_collection("法律の種類")
+    def store(self,chunk_text:str,embedding:list,doc_id:str):
+        self.collection.add(
+            ids=[doc_id],
+            embeddings=[embedding],
+            documents=[chunk_text]
+        )
+    def query(self,query_embedding:list,n_results: int = 5):
+        results = self.collection.query(
+            query_embeddings=[query_embedding],
+            n_results=n_results
+        )
+        return results["documents"][0]
