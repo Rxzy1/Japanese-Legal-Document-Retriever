1c6e04df585f205ae630e5d8690c7acdc33b12f9
Tushar Mudgal <tusharmudgal669@gmail.com>
2026-04-13 07:44:13 +0530

    Added a function to create tokenized chunks

---
+++ core/chunks.py
@@ -0,0 +1,36 @@
+class Chunks:
+    def __init__(self,max_size):
+        self.max_size = 200
+        self.chunk=[]
+    def create_chunks(self,text:list) -> list:
+        temp = []
+        self.chunk = []
+        count = 0
+        for i in range(len(text)):
+            temp.append(text[i])
+            count+=1
+            if text[i]=='。':
+                self.chunk.append("".join(temp))
+                temp = []
+                count=0
+            elif count==self.max_size:
+                for j in range(len(temp)-1,-1,-1):
+                    if temp[j]=="，" or temp[j]=="、":
+                        leftover = temp[j+1:]
+                        temp=temp[:j+1]
+                        self.chunk.append("".join(temp))
+                        temp = leftover
+                        count = len(temp)
+                        break
+                else:
+                    self.chunk.append("".join(temp))
+                    temp = []
+                    count=0
+        if temp:
+            self.chunk.append("".join(temp))
+        return self.chunk
+
+
+
+
+
