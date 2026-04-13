88dcaba554bbbb469162e3c38c2b9167791bd14f
Tushar Mudgal <tusharmudgal669@gmail.com>
2026-04-13 06:08:09 +0530

    Added Japanese tokenizer

--- core/sudachi.py
+++ core/sudachi.py
@@ -1,2 +1,14 @@
+
+
+from sudachipy import Dictionary,SplitMode
+
 class Sudachi:
-    pass()
+    def __init__(self):
+        self.tokenizer_obj = Dictionary(dict="full").create()
+        self.mode = SplitMode.C
+    def token_words(self,text:str) -> list:
+        if not text:
+            return []
+        tokens = self.tokenizer_obj.tokenize(text,self.mode)
+        return [t.surface() for t in tokens if t.surface().strip()]
+
