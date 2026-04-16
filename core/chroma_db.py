033c9e3f629ae43443323d4ea2d781b4da39ca2f
Tushar Mudgal <tusharmudgal669@gmail.com>
2026-04-16 22:36:28 +0530

    added a retreival system

--- core/chroma_db.py
+++ core/chroma_db.py
@@ -1,17 +1,22 @@
 import chromadb
+from importlib_metadata import metadata
+
+
 class store_vector:
     def __init__(self):
         self.client = chromadb.PersistentClient(path=r"C:\Project\Vectors")
         self.collection = self.client.get_or_create_collection("legal_documents")
-    def store(self,chunk_text:str,embedding:list,doc_id:str):
-        self.collection.add(
-            ids=[doc_id],
-            embeddings=[embedding],
-            documents=[chunk_text]
+    def store(self,chunk_text:list,embedding:list,doc_id:list,meta_data:list):
+        self.collection.upsert(
+            ids=doc_id,
+            embeddings=embedding,
+            documents=chunk_text,
+            metadatas=meta_data,
         )
-    def query(self,query_embedding:list,n_results: int = 5):
+    def query(self,query_embedding:list,n_results: int = 5,where: dict=None):
         results = self.collection.query(
             query_embeddings=[query_embedding],
-            n_results=n_results
+            n_results=n_results,
+            where=where,
         )
         return results["documents"][0]
